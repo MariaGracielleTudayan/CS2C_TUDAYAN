1e49bd6569294432bd630b07896a9015bcc308b2
MariaGracielleTudayan <145459165+MariaGracielleTudayan@users.noreply.github.com>
2023-10-18 06:49:09 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// Activity no. 1_Maria Gracielle G. Tudayan_Muryot;
+let name = "Maria Gracielle Tudayan";
+let gender = "Female";
+let fbName = "Gracielle Tudayan";
+
+console.log(name);
+console.log(gender);
+console.log(fbName);
---
+++ boolean.js
@@ -0,0 +1,9 @@
+let isEnrolledInDSA = true;
+let isEnrolledInArt = false;
+let yearToday = "2023";
+let birthDate = "2004";
+let age = yearToday - birthDate;
+
+console.log(isEnrolledInDSA);
+console.log(isEnrolledInArt);
+console.log(age);
